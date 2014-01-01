362e1f8383376ce9e65856298b24f00c28b596bc
Marco Godínez <markotom@gmail.com>
2014-01-01 05:24:06 -0600

    Fixed view of gelling meta box

--- inc/views/jelly_boxes/gelling.php
+++ inc/views/jelly_boxes/gelling.php
@@ -1,6 +1,6 @@
 <div class="columns-prefs">
-  <label><input name="jelly_gelling[grenetina]" type="checkbox" value="true" <?php if( $gelling && $gelling['grenetina'] ) echo 'checked="checked"' ?>>Grenetina</label>
-  <label><input name="jelly_gelling[agar]" type="checkbox" value="true" <?php if( $gelling && $gelling['agar'] ) echo 'checked="checked"' ?>>Agar</label>
+  <label><input name="jelly_gelling[0]" type="checkbox" value="Grenetina" <?php if( $gelling && $gelling['0'] ) echo 'checked="checked"' ?>>Grenetina</label>
+  <label><input name="jelly_gelling[1]" type="checkbox" value="Agar" <?php if( $gelling && $gelling['1'] ) echo 'checked="checked"' ?>>Agar</label>
 </div>
 
 <input type="hidden" name="jelly_gelling_nonce" id="jelly_gelling_nonce" value="<?php echo $nonce; ?>">
